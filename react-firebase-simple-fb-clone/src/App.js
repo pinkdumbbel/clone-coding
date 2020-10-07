aada3eec457606e7ee8370a49a14b19b971ea25d
CHOI <tlacjd665@gmail.com>
2020-10-07 20:39:37 +0900

    페이스북 클론 코딩 웹팩으로 리액트 실행환경 구축 및 파이어 베이스 설정

---
+++ react-firebase-simple-fb-clone/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div> Hello! Fb Clone Coding!</div>
+    );
+}
+
+export default App;
---
+++ react-firebase-simple-fb-clone/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import firebase from './firebase';
+
+console.log(firebase);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
