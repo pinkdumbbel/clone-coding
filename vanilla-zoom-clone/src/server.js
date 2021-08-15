8cb54662a2b9c5193d95d69d9a018408d3e5f705
CHOI <tlacjd665@gmail.com>
2021-08-15 17:35:19 +0900

    닉네임과 실제 메세지 실시간 통신 구현

--- vanilla-zoom-clone/src/server.js
+++ vanilla-zoom-clone/src/server.js
@@ -1,4 +1,5 @@
-import express from 'express';
+import { Socket } from 'dgram';
+import express, { json } from 'express';
 import http from 'http';
 import WebSocket from 'ws';
 
@@ -13,8 +14,28 @@ app.get('/*', (req, res) => res.redirect('/'));
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (socket) => console.log(socket));
+const sockets = [];
+wss.on('connection', (socket) => {
+    socket['nickname'] = 'anonymous';
+    sockets.push(socket);
+    console.log('Connected to Browser');
+    //브라우저로부터 요청 받음
+    socket.on('message', (message) => {
+        const parsed = JSON.parse(message);
+        switch (parsed.type) {
+            case 'nickname':
+                socket['nickname'] = parsed.payload;
+                break;
+            case 'message':
+                //브라우저에 응답 보냄
+                sockets.forEach((soc) => soc.send(`${socket.nickname}: ${parsed.payload}`));
+                break;
+        }
+    });
+    socket.on('close', () => console.log('Disconnected to Browser'));
+});
 server.listen(3000);
 console.log('...listening http://localhost:3000/');
+
+
 
